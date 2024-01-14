9348d4e6c94349590b0e15fd6364aa94c4ac26ea
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-01-15 02:45:53 +0530

    Stable Base

--- gin.go
+++ gin.go
@@ -1,13 +1,43 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func mysqlPoolConnection() (*sql.DB, error) {
+	// db, err := sql.Open("mysql", "root:Aeiou@1234/BookSchema")
+    db, err := sql.Open("mysql", "root:Aeiou@1234@tcp(127.0.0.1:3306)/BookSchema")
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	// I will refer the below link for the above db methods
+	// https://github.com/go-sql-driver/mysql#important-settings
+
+	return db, nil
+}
 
 func main() {
-	route := gin.Default()
-	route.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	route.Run() // listen and serve on 0.0.0.0:8080
+	db, err := mysqlPoolConnection()
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		fmt.Println("Error pinging the database:", err)
+		return
+	}
+
+	fmt.Println("Successfully connected to the database")
 }
